ed1daebc442ce22276930785c53e3537e2605b54
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-13 02:47:30 -0300

    s

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,21 +1,46 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from auth import auth_blueprint
 from users import users_blueprint
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import os
 
+# Inicializa o Flask app
 app = Flask(__name__)
+# Verifica o ambiente e configura a URI do banco de dados
+if os.environ.get('FLASK_ENV') == 'production':
+    # Para produção com PostgreSQL
+    DATABASE_URI = 'postgresql+psycopg2://flask_user:tonyteste@147.93.12.171:5432/northcromo'
+else:
+    # Para desenvolvimento com SQLite
+    DATABASE_URI = 'sqlite:///northcromo.db'
 
-# Configurações do banco de dados e outras configurações
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://seu_usuario:sua_senha@localhost/nome_do_banco'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desabilitar tracking de modificações
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# Criar o engine diretamente com SQLAlchemy
+engine = create_engine(DATABASE_URI)
 
-# Registra os blueprints
+# Criar a fábrica de sessões manualmente (sessionmaker)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Inicializa Migrate para gerenciar migrations (opcional, caso queira usar)
+migrate = Migrate(app, engine)
+
+# Testa a conexão com o banco de dados
+try:
+    with engine.connect() as conn:
+        # Executa a query de teste
+        conn.execute(text('SELECT 1'))
+    print("Conexão com o banco de dados bem-sucedida!")
+except Exception as e:
+    print(f"Erro ao conectar com o banco de dados: {e}")
+
+# Registra blueprints
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(users_blueprint, url_prefix='/users')
 
+# Ponto de entrada
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
