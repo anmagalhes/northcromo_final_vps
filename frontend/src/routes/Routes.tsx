89c378b43899d12ec4e283f3abb7e0e018d363de
root <root@srv668340.hstgr.cloud>
2024-12-15 20:37:22 +0000

    Mensage

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,13 +1,11 @@
 // src/routes/Routes.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';  // Certifique-se de que este arquivo está correto
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
