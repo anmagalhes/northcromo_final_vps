338af8c2eaeeddc7d77814298fbc5aed7d89bbbd
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 01:14:59 -0300

    s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,20 +9,19 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@utils': path.resolve(__dirname, './src/utils'),
-    }
+    },
   },
   server: {
-    port: 3000, // Porta do servidor de desenvolvimento
+    port: 3000,
     proxy: {
-      // Para desenvolvimento, se a API estiver rodando em localhost:5000
-      '/api': 'http://localhost:5000', // Substitua localhost:5000 pela sua API local
+      '/api': 'http://localhost:5000', // Apenas para desenvolvimento
     },
   },
-  // Configuração para produção
   build: {
-    // Em produção, as requisições irão para o domínio correto
+    // Produção usa variáveis de ambiente
     define: {
-      'process.env.API_URL': JSON.stringify('https://northcromocontrole.com.br/api'),
+      'process.env.API_URL': JSON.stringify(process.env.VITE_API_URL),
     },
   },
-})
+});
+
