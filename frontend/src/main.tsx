cb33df42bf2a9ddce2e53509ae88315b2087c4ba
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 16:36:57 -0300

    e

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
+import './styles/styles.css';  // Importe o CSS local
 
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement); // Cria o root usando createRoot
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
