d7eec02a27b3d2688083ab46d0042696e71cd76e
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 16:59:21 -0300

    a

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <header className={styles.header}>  {/* Remover as aspas ao redor de {styles.header} */}
       <h1>COMO VAI AMOR ?</h1>
     </header>
   );
