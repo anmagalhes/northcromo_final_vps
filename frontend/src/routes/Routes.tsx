f3a8b71efd9c03eb6d4938818aea3ce58de00c23
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 16:14:14 -0300

    D

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,7 +1,7 @@
 // src/routes/Routes.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Home } from 'pages/home';  // Certifique-se de que Home está corretamente importado
+import { Home } from 'pages/Home';  // Certifique-se de que Home está corretamente importado
 import { Cliente } from 'pages/Cliente'; // Da mesma forma, Cliente deve ser importado corretamente
 
 export const AppRoutes: React.FC = () => {
