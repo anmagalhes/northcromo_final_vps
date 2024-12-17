3789a72522ebd70c7921c35c67aa7684e1334c3b
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 22:05:30 -0300

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom'; // React Router
+import { Routes, Route } from 'react-router-dom'; // React Router
 
 // Components
-import Header from  './components/Header/Header';
+import Header from './components/Header/Header';
 import Footer from './components/Footer/Flooter';
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar';
 
 // Importando as rotas
 import AppRoutes from './routes/Routes';
@@ -15,27 +15,25 @@ import styles from "./App.module.css"; // Estilos locais
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>  {/* Contorna a aplicação com o React Router */}
-      <div className={styles.appContainer}> {/* Contêiner principal */}
+    <div className={styles.appContainer}> {/* Contêiner principal */}
 
-        {/* Cabeçalho */}
-        <Header />
+      {/* Cabeçalho */}
+      <Header />
 
-        <div className={styles.mainLayout}>  {/* Layout com sidebar e conteúdo */}
+      <div className={styles.mainLayout}>  {/* Layout com sidebar e conteúdo */}
 
-          {/* Menu Lateral (Sidebar) */}
-          <Sidebar />
+        {/* Menu Lateral (Sidebar) */}
+        <Sidebar />
 
-          {/* Conteúdo Principal */}
-          <main className={styles.mainContent}>  {/* Principal área de conteúdo */}
-            <AppRoutes /> {/* Renderiza as rotas */}
-          </main>
-        </div>
-
-        {/* Rodapé */}
-        <Footer />
+        {/* Conteúdo Principal */}
+        <main className={styles.mainContent}>  {/* Principal área de conteúdo */}
+          <AppRoutes /> {/* Renderiza as rotas */}
+        </main>
       </div>
-    </BrowserRouter>
+
+      {/* Rodapé */}
+      <Footer />
+    </div>
   );
 }
 
