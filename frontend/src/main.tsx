2ed8f2bdca7db91655bf15108d176754062b2698
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 21:47:07 -0300

    16.12.2024

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
+// main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';  // Usando a versão moderna da React 18+
+import './index.css';
 import './styles/styles.css';  // Importe o CSS local
 
 import App from './App';
 
+// Criação da raiz para o React 18+
 const rootElement = document.getElementById('root') as HTMLElement;
-const root = ReactDOM.createRoot(rootElement); // Cria o root usando createRoot
+const root = createRoot(rootElement);  // Usando createRoot
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
