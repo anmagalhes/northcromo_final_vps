66c653c49cd0f7164838cba0fd3cca8ee9d4027f
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-17 02:44:52 -0300

    17.12.2024

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,8 +3,8 @@
 # Caminho do seu diretório de projeto
 PROJECT_DIR="/var/www/northcromo_final_vps"
 
-# Caminho do ambiente virtual (confirmado que o diretório correto é /var/www/northcromo_final_vps/backend/venv)
-VENV_DIR="$PROJECT_DIR/backend/venv"  # Este caminho já está correto
+# Caminho do ambiente virtual
+VENV_DIR="$PROJECT_DIR/backend/venv"
 
 # Caminho do Gunicorn
 GUNICORN_SERVICE="/etc/systemd/system/gunicorn.service"
@@ -21,10 +21,9 @@ echo "Puxando as últimas alterações do Git..."
 git pull origin main || { echo "Erro: Falha ao puxar do Git. Verifique se o repositório remoto está configurado corretamente."; exit 1; }
 
 # Passo 3: Verificar e ativar o ambiente virtual
-echo "Verificando o ambiente virtual em: $VENV_DIR"
 if [ -f "$VENV_DIR/bin/activate" ]; then
-    echo "Ambiente virtual encontrado. Ativando..."
-    source $VENV_DIR/bin/activate || { echo "Erro: Não foi possível ativar o ambiente virtual."; exit 1; }
+    echo "Ativando o ambiente virtual..."
+    source $VENV_DIR/bin/activate
 else
     echo "Erro: Ambiente virtual não encontrado em $VENV_DIR"
     exit 1
@@ -34,7 +33,7 @@ fi
 echo "Instalando as dependências do projeto..."
 pip install -r $BACKEND_DIR/requirements.txt || { echo "Erro: Falha ao instalar as dependências."; exit 1; }
 
-# Passo 5: Reiniciar o Gunicorn
+# Passo 5: Reiniciar o Gunicorn (caso você esteja utilizando o systemd)
 echo "Reiniciando o Gunicorn..."
 systemctl restart gunicorn || { echo "Erro: Falha ao reiniciar o Gunicorn."; exit 1; }
 
@@ -43,3 +42,4 @@ echo "Verificando o status do Gunicorn..."
 systemctl status gunicorn --no-pager || { echo "Erro: Gunicorn não está funcionando corretamente."; exit 1; }
 
 echo "Deploy realizado com sucesso!"
+
