f94da461d34fa8640347f2e1712446e63612f48c
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-16 21:29:11 -0300

    16.12.2024

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,21 +2,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from '@pages/Home';  // Usando o alias
-import Cliente from '@pages/Cliente';  // Usando o alias
+//import Home from '../pages/Home';  // Usando o alias
+import Cliente from '../pages/Cliente';  // Usando o alias
 
 export const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         {/* Rota para o componente Home */}
-        <Route path="/home" element={<Home />} />
         
         {/* Rota para o componente Cliente */}
         <Route path="/cliente" element={<Cliente />} />
 
         {/* Redirecionamento para /home caso a rota não exista */}
-        <Route path="*" element={<Navigate to="/cliente" />} />
+        <Route path="/" element={<Navigate to="/cliente" />} />
       </Routes>
     </Router>
   );
