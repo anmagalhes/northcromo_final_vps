94213229c98fe577c0ee0d6a7bab406a25c7b806
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-18 20:27:03 -0300

    ad

--- backend/app/app.py
+++ backend/app/app.py
@@ -4,7 +4,6 @@ import os
 # Ajuste do Python Path para garantir que o diretório correto seja encontrado
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import asyncio
 from flask import Flask, jsonify, g
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -44,23 +43,22 @@ async def initialize_database():
     return engine
 
 # Função principal de criação do app
-async def create_app():
-    await initialize_database()  # Inicializa o banco de dados de forma assíncrona
+def create_app():
+    # Criação do app Flask
+    app = Flask(__name__)
+    CORS(app)
     
-    # Registra o blueprint do frontend
-    app.register_blueprint(frontend_bp)  # Registra o blueprint do frontend
     # Registra os blueprints
-    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')  # Rotas de autenticação
-    app.register_blueprint(users_blueprint, url_prefix='/api/users')  # Rotas de usuários
-    app.register_blueprint(defeito_blueprint, url_prefix='/api/defeitos')  # Rotas de defeitos
+    app.register_blueprint(frontend_bp)
+    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
+    app.register_blueprint(users_blueprint, url_prefix='/api/users')
+    app.register_blueprint(defeito_blueprint, url_prefix='/api/defeitos')
     app.register_blueprint(checklist_recebimento_blueprint, url_prefix='/api/checklist')
     app.register_blueprint(foto_recebimento_blueprint, url_prefix='/api/foto_recebimento')
     app.register_blueprint(funcionario_blueprint, url_prefix='/api/funcionario')
     app.register_blueprint(cliente_blueprint, url_prefix='/api/clientes')
     app.register_blueprint(componente_blueprint, url_prefix='/api/componente')
-    #app.register_blueprint(grupo_produto_blueprint, url_prefix='/grupo_produto')
-
-    # Exemplo de rota da API
+    
     @app.route('/api')
     def api_home():
         return jsonify({
@@ -68,25 +66,21 @@ async def create_app():
             "message": "Welcome to the API endpoint!"
         })
     
-     # Adiciona um tratamento de erro global
     @app.errorhandler(Exception)
     def handle_error(e):
-        # Loga o erro (pode ser ajustado para salvar os logs em um arquivo ou sistema de logs)
         print(f"Error: {str(e)}")
-        
-        # Retorna uma resposta amigável ao usuário
         return jsonify({
             "status": "error",
             "message": "An unexpected error occurred. Please try again later."
-        }), 500  # Retorna código de erro 500 (erro interno do servidor)
-
+        }), 500
+    
     return app
 
 # Funções para abrir e fechar a sessão do banco de dados para cada requisição
 @app.before_request
 def before_request():
     """Abre a sessão do banco de dados antes de cada requisição"""
-    g.db_session = db.session()  # Usando o sessionmaker para abrir a conexão
+    g.db_session = db.session()
 
 @app.after_request
 def after_request(response):
@@ -99,12 +93,8 @@ def after_request(response):
         g.db_session.close()  # Fecha a sessão do banco de dados
     return response
 
-
-# RODA EM PRODUÇÃO 
-
-# Função principal para rodar a aplicação
+# Função principal para rodar a aplicação em produção com Gunicorn
 if __name__ == "__main__":
-    # Rodar o app com Gunicorn no ambiente de produção (não use `app.run()`)
     from gunicorn.app.base import BaseApplication
 
     class GunicornApp(BaseApplication):
@@ -126,26 +116,13 @@ if __name__ == "__main__":
             for key, value in config.items():
                 self.cfg.set(key, value)
 
-    # Criação do app Flask
+    # Inicializa o banco de dados de forma assíncrona
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(create_app())  # Cria o app assíncrono
+    loop.run_until_complete(initialize_database())  # Inicia o banco de dados
+
+    # Criação do app Flask
+    app = create_app()
 
     # Inicializa o Gunicorn
     gunicorn_app = GunicornApp(app)
     gunicorn_app.run()
-
-
-# RODA EM DEV
-
-# Função para rodar a aplicação - DEV
-#def run():
-    # Usamos asyncio.run para inicializar o app de forma assíncrona
-#    asyncio.run(start_app())  # Inicia a aplicação assíncrona
-
-# Função para inicializar o app com Flask
-#async def start_app():
-#    app = await create_app()  # Cria a aplicação Flask com configuração assíncrona
-#    app.run(host="0.0.0.0", port=5000, debug=False) # Rodando Flask de maneira síncrona, mas com setup assíncrono
-
-#if __name__ == "__main__":
-#     run()  # Inicia o processo de execução
