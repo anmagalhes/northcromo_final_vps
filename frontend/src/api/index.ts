0904eb3f878da53ec2a77ff3fbf325e5cba5f89c
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-19 13:20:08 -0300

    19.12.2024

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 // Definir a URL base da API dependendo do ambiente
 const API_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:5000/api'  // URL para desenvolvimento (Flask rodando localmente na porta 5000)
-  : '/api';  // URL relativa para produção (quando o frontend e o backend estão no mesmo domínio)
+  : 'https://northcromocontrole.com.br/api;  // URL relativa para produção (quando o frontend e o backend estão no mesmo domínio)
 
 export const fetchAPI = async (endpoint: string, method: string = 'GET', data: any = null) => {
   const options: RequestInit = {
