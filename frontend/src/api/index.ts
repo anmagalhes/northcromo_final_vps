5e8863b3b0a8004f9a395c36459949940f0e6d2b
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-19 13:14:50 -0300

    19.12.2024

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,32 +1,31 @@
-// src/api/index.ts
+// Definir a URL base da API dependendo do ambiente
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000/api'  // URL para desenvolvimento (Flask rodando localmente na porta 5000)
+  : '/api';  // URL relativa para produção (quando o frontend e o backend estão no mesmo domínio)
 
-const API_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:5000/api'  // URL para desenvolvimento
-  : '/api';  // URL relativa para produção
-
-// Função para realizar as requisições HTTP
 export const fetchAPI = async (endpoint: string, method: string = 'GET', data: any = null) => {
-    const options: RequestInit = {
-        method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
+  const options: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    if (data) {
-        options.body = JSON.stringify(data);  // Se houver dados, adiciona ao corpo da requisição
+  if (data) {
+    options.body = JSON.stringify(data);  // Se houver dados, adiciona ao corpo da requisição
+  }
+
+  try {
+    const response = await fetch(`${API_URL}${endpoint}`, options);
+
+    if (!response.ok) {
+      throw new Error('Erro na requisição');
     }
 
-    try {
-        const response = await fetch(`${API_URL}${endpoint}`, options);
-        
-        if (!response.ok) {
-            throw new Error('Erro na requisição');
-        }
-
-        return await response.json();  // Retorna o corpo da resposta JSON
-    } catch (error) {
-        console.error('Erro ao fazer a requisição:', error);
-        throw error;
-    }
+    return await response.json();  // Retorna o corpo da resposta JSON
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error);
+    throw error;
+  }
 };
+
