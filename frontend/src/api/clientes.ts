f2b88021270ec434e022f8482119c17e570009e6
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-19 12:57:10 -0300

    19.12.2024

--- frontend/src/api/clientes.ts
+++ frontend/src/api/clientes.ts
@@ -3,20 +3,20 @@ import { fetchAPI } from './index';
 
 // Função para obter a lista de clientes
 export const getClientes = async () => {
-    return fetchAPI('/clientes');
+    return fetchAPI('/api/cliente');
 };
 
 // Função para criar um novo cliente
 export const createCliente = async (clienteData: any) => {
-    return fetchAPI('/clientes', 'POST', clienteData);
+    return fetchAPI('/api/cliente', 'POST', clienteData);
 };
 
 // Função para atualizar os dados de um cliente
 export const updateCliente = async (clienteId: number, clienteData: any) => {
-    return fetchAPI(`/clientes/${clienteId}`, 'PUT', clienteData);
+    return fetchAPI(`/api/cliente/${clienteId}`, 'PUT', clienteData);
 };
 
 // Função para deletar um cliente
 export const deleteCliente = async (clienteId: number) => {
-    return fetchAPI(`/clientes/${clienteId}`, 'DELETE');
+    return fetchAPI(`/api/cliente/${clienteId}`, 'DELETE');
 };
