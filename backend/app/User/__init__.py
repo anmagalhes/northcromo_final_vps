ee80bc38f7641ffbd03c6cdf5c7ff45e2aa70026
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-19 02:53:33 -0300

    aq

--- backend/app/User/__init__.py
+++ backend/app/User/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 # Criar o Blueprint para usuários
 users_blueprint = Blueprint('user', __name__)
 
-# Importar as rotas do arquivo routes.py
+# Importar as rotas do arquivo routes.pyss
 from . import routes
