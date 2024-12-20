40edbe3e04fb7e48ce3a2bafae24e6c9000616c0
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-20 00:36:38 -0300

    20.12.2024

--- frontend/src/pages/cliente.tsx
+++ frontend/src/pages/cliente.tsx
@@ -11,37 +11,25 @@ const Cliente: React.FC = () => {
 
   // Carregar os clientes do localStorage com expiração de 24 horas
   useEffect(() => {
-    try {
-      // Carrega os dados dos clientes e verifica a expiração (24h)
-      const clientesCarregados = carregarComVerificacaoDeExpiracao('clientes', 24 * 60 * 60 * 1000); // 24 horas em ms
+    const clientesCarregados = carregarClientes();  // Recupera os clientes do localStorage
 
-      if (clientesCarregados) {
-        setClientes(clientesCarregados);  // Carrega os dados para o estado
-      } else {
-        setClientes([]);  // Caso não haja dados ou os dados estejam expirados
-      }
-    } catch (error) {
-      console.error("Erro ao carregar os clientes do localStorage:", error);
-      setClientes([]);  // Em caso de erro, inicializa como um array vazio
+    if (clientesCarregados.length > 0) {
+      setClientes(clientesCarregados);  // Carrega os dados para o estado
+    } else {
+      setClientes([]);  // Caso não haja dados ou os dados estejam expirados
     }
   }, []);  // O useEffect será executado uma única vez no carregamento da página
 
   // Função para adicionar um cliente
   const handleClienteAdicionado = (novoCliente: Cliente) => {
     try {
-      // Recupera os clientes existentes do localStorage
-      const clientesExistentes = JSON.parse(localStorage.getItem('clientes') || '[]');
-
-      // Adiciona o novo cliente ao array de clientes existentes
-      const updatedClientes = [...clientesExistentes, novoCliente];
+      // Adiciona o novo cliente ao localStorage
+      adicionarClienteAoLocalStorage(novoCliente);
 
       // Atualiza o estado com o novo array de clientes
-      setClientes(updatedClientes);
-
-      // Salva o array atualizado no localStorage com expiração (24h)
-      salvarComExpiracao('clientes', updatedClientes, 24 * 60 * 60 * 1000);  // Salvando com expiração de 24 horas
+      setClientes((prevClientes) => [...prevClientes, novoCliente]);
     } catch (error) {
-      console.error("Erro ao adicionar cliente ao localStorage:", error);
+      console.error("Erro ao adicionar cliente:", error);
     }
   };
 
@@ -52,7 +40,7 @@ const Cliente: React.FC = () => {
       const updatedClientes = clientes.filter((cliente) => cliente.id !== id);
       setClientes(updatedClientes);  // Atualiza a lista de clientes no estado
 
-      // Salva o array atualizado no localStorage com expiração (24h)
+      // Salva o array atualizado no localStorage
       salvarComExpiracao('clientes', updatedClientes, 24 * 60 * 60 * 1000);  // Salvando com expiração de 24 horas
     } catch (error) {
       console.error("Erro ao excluir cliente:", error);
