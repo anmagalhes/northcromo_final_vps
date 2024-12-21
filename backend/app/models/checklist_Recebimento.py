b9b29946e13f144b568e1eb81290f68b50dc570a
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-21 05:33:55 -0300

    de

--- backend/app/models/checklist_Recebimento.py
+++ backend/app/models/checklist_Recebimento.py
@@ -24,9 +24,9 @@ class ChecklistRecebimento(db.Model):
     cliente = relationship("Cliente", back_populates="checklists")
     produto = relationship("Produto", back_populates="checklists")
     usuario = relationship("User", back_populates="checklists")
-    impressao_checklists = relationship("ImpressaoChecklistRecebimento", backref="checklist")
-    checklist = db.relationship('ChecklistRecebimento', backref='impressao_checklists')
-
+    
+    # Remove o relacionamento redundante com 'checklist'
+    impressao_checklists = relationship("ImpressaoChecklistRecebimento", backref="checklist", lazy=True)
 
     # Colunas de data e hora
     created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Data de criação
