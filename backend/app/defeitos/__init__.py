f119d2204ab30adf9f2ade0d4035e2a3e2be10a2
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-22 15:46:06 -0300

    as

--- backend/app/defeitos/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# Cria o blueprint para o módulo defeitos
-defeito_blueprint = Blueprint('defeito', __name__)
-
-# Importa as rotas que serão registradas no blueprint
-from . import routes
-
