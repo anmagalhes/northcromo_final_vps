405b3ee59efd279222a2acdda9eabb4995325094
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-24 01:23:20 -0300

    ed

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,8 @@
-# app/__init__.pypip install flask-cors
+# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS  # Importe o CORS
 from app.config import DevelopmentConfig
 
 # Inicializando as instâncias de db e migrate globalmente
@@ -15,8 +16,9 @@ def create_app():
     db.init_app(app)  # Inicializa o banco de dados com a configuração
     migrate.init_app(app, db)  # Inicializa o Flask-Migrate
 
-    # Configurar o CORS
-    CORS(app)  # Isso vai permitir CORS para toda a aplicação
+    # Configurar o CORS para permitir todas as origens
+    CORS(app, resources={r"/*": {"origins": "*"}})  # Permite todas as origens para toda a aplicação
 
     return app
+
 
--- backend/app/core/__init__.py
+++ backend/app/core/__init__.py
@@ -1,14 +1,5 @@
 # app/cors/__init__.py
 from flask_cors import CORS
-#from typing import List
-
-#from pydantic import BaseSettings, AnyHttpUrl
-#from sqlalchemy.ext.declarative import declarative_base
-
-
-#class Settings(BaseSettings):
-
-    #API_V1_STR: str = '/ap'
 
 def configure_cors(app):
     CORS(app)
