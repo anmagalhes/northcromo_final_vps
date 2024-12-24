11c621ea5280b56ea872a93d1bf5322eb2aedcd1
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-24 01:11:08 -0300

    analisando

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS  # Importando o CORS
 from app.config import DevelopmentConfig
 
 # Inicializando as instâncias de db e migrate globalmente
