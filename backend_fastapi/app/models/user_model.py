bd8cc1a0da4f080f29b14da4a09f71677e005845
Antonio Melo Magalhaes <anmagalhaes@solarbr.com.br>
2024-12-31 23:53:12 -0300

    TESTE_USUARIO

--- backend_fastapi/app/models/user_model.py
+++ backend_fastapi/app/models/user_model.py
@@ -24,7 +24,7 @@ def get_current_time_in_sp() -> datetime:
     return datetime.now(SP_TZ).astimezone(
         SP_TZ
     )  # Garante que a data e hora sejam "aware"
-
+# TESTE
 
 # Alternativa: utilizar UTC
 def get_current_time_in_utc() -> datetime:
